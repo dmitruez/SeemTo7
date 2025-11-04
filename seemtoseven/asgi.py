8000a09c48f7720092567ff4bba7c85c6b1c225c
Mitya <148676835+dmitruez@users.noreply.github.com>
2025-11-05 00:01:27 +0300

    Initialize Django project scaffolding

---
+++ seemtoseven/asgi.py
@@ -0,0 +1,11 @@
+"""ASGI config for seemtoseven project."""
+
+from __future__ import annotations
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seemtoseven.settings")
+
+application = get_asgi_application()
